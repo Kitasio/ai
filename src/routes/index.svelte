8b606614285717dda25c3f878d26ea0200c8f992
batumi <batumi@batumis-MacBook-Pro.local>
2022-03-17 22:41:08 +0300

    colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,10 @@
 	let prompt;
 	let max_tokens;
 	let data;
+  let loading = false;
 
 	const send = async () => {
+    loading = true;
 		const res = await fetch('api/oai', {
 			method: 'POST',
 			headers: {
@@ -13,37 +15,45 @@
 			body: JSON.stringify({ prompt, max_tokens })
 		});
 
+    loading = false;
 		data = await res.json();
 		data = data.data;
 	};
 </script>
 
-<div class="h-screen bg-emerald-500">
-	<h1 class="text-emerald-200 text-5xl text-center pt-5">OAI</h1>
-	<div class="flex flex-col justify-center gap-5 mt-10 max-w-6xl md:mx-auto mx-5">
-		<textarea
-			bind:value={prompt}
-			type="text"
-			class="rounded-xl text-emerald-100 p-2 bg-emerald-500 border-2 border-emerald-50"
-		/>
-		<div class="flex justify-between items-center">
-			<input
-				bind:value={max_tokens}
-				type="number"
-				class="rounded-xl text-emerald-100 p-2 bg-emerald-500 border-2 border-emerald-50 w-32"
+<div class="h-screen bg-slate-900">
+	<h1 class="text-pink-600 text-5xl text-center pt-5">OAI</h1>
+	<form on:submit|preventDefault={send}>
+		<div
+			class="flex flex-col font-mono text-lg justify-center gap-5 mt-10 max-w-screen-md md:mx-auto mx-5 sm:mx-16"
+		>
+			<textarea
+				bind:value={prompt}
+				type="text"
+				class="rounded-xl caret-pink-500 text-pink-500 focus:outline-none p-2 bg-transparent border-2 border-pink-500"
 			/>
-			<div
-				on:click={send}
-				class="border-2 uppercase hover:scale-105 transition duration-200 cursor-pointer border-emerald-50 p-2 rounded-xl text-emerald-100"
-			>
-				click
+			<div class="flex justify-between md:justify-start md:gap-5 items-center">
+				<input
+					bind:value={max_tokens}
+					type="number"
+					class="rounded-xl focus:outline-none text-pink-500 p-2 bg-transparent border-2 border-pink-500 w-32"
+				/>
+				<button
+          type="submit"
+					class="border-2 hover:scale-105 transition duration-200 cursor-pointer border-pink-500 p-2 rounded-xl text-pink-500"
+				>
+					Submit
+				</button>
 			</div>
 		</div>
-	</div>
 
-	{#if data}
-		<div in:fade class="mt-5 text-emerald-100 mx-5 md:mx-auto max-w-6xl">
-			{@html data}
-		</div>
-	{/if}
+    {#if loading}
+      <div class="text-pink-500 font-mono text-lg max-w-screen-md md:mx-auto">Loading...</div>
+    {/if}
+		{#if data}
+			<div in:fade class="mt-5 text-lg font-mono text-pink-500 mx-5 md:mx-auto max-w-screen-md">
+				{@html data}
+			</div>
+		{/if}
+	</form>
 </div>
