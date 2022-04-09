d4a3b3c59f35b18efae7f31d820791cd9db64fd6
soulgenesis <soulgen@protonmail.com>
2022-04-09 13:11:31 +0300

    adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
 
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '200.html'
+		})
 	}
 };
-
-export default config;
